edaab1c75c06edfec1d843ca08a9ea73ae573d73
rik <rikdegraeve@gmail.com>
2024-05-04 16:12:30 +0200

    :w added lsp server

--- lua/rik/lazy.lua
+++ lua/rik/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("rik.plugins", {
+require("lazy").setup({{ import = "rik.plugins" }, {import = "rik.plugins.lsp"} }, {
   checker = {
     enabled = true,
     notify = false,
